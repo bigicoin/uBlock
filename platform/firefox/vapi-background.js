75f906dbf1799f0d19f0ad93b0ef6d84f6459244
Bigi Lui <bigi@cointent.com>
2017-01-13 23:03:35 -0800

    Prepping a Firefox version of simpleBlock, with updated icons and name. Don't know yet if addons.mozilla.org will approve this, but worth a shot.

--- platform/firefox/vapi-background.js
+++ platform/firefox/vapi-background.js
@@ -76,7 +76,7 @@ var deferUntil = function(testFn, mainFn, details) {
 /******************************************************************************/
 
 vAPI.app = {
-    name: 'uBlock Origin',
+    name: 'simpleBlock',
     version: location.hash.slice(1)
 };
 
